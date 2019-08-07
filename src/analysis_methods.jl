be29842fe22595f192e946186312e9e98298c8c1
Cameron Perot <cameron.perot@protonmail.ch>
2019-08-07 19:50:17 +0200

    Updated analysis methods

--- src/analysis_methods.jl
+++ src/analysis_methods.jl
@@ -1,12 +1,11 @@
 """
 	compute_Δ_method_1!(g::AbstractGraph)
 
-Determines `Δ`, `t₀`, and `t₁` for the given evolved `g`
+Determines `t₀` and `t₁` for the given evolved `g`
 
 Arguments
 * `g` : An evolved instance of type AbstractGraph
 Extra Information
-* `Δ` : `t₁ - t₀`
 * `t₀`: The last step in the evolution process satisfying `g.observables.largest_cluster_size < sqrt(g.n)`
 * `t₁`: The first step in the evolution process satisfying `g.observables.largest_cluster_size > 0.5g.n`
 Returns
@@ -15,19 +14,20 @@ Returns
 function compute_Δ_method_1!(g::AbstractGraph)
 	t₀ = sum(g.observables.largest_cluster_size .< sqrt(g.n))
 	t₁ = sum(g.observables.largest_cluster_size .<= 0.5g.n) + 1
-	g.observables.Δ_method_1 = (t₁-t₀, t₀, t₁) ./ g.n
+	C₀ = g.observables.largest_cluster_size[t₀]
+	C₁ = g.observables.largest_cluster_size[t₁]
+	g.observables.Δ_method_1 = (t₀, t₁, C₀, C₁) ./ g.n
 end
 
 
 """
 	compute_Δ_method_2!(g::AbstractGraph)
 
-Determines `Δ`, `t₀`, and `t₁` for the given evolved `g`
+Determines `t₀` and `t₁` for the given evolved `g`
 
 Arguments
 * `g` : An evolved instance of type AbstractGraph
 Extra Information
-* `Δ` : `t₁ - t₀`
 * `t₀`: The step in the evolution process where the heterogeneity peaks
 * `t₁`: The step in the evolution process where the size of the largest cluster increases the most
 Returns
@@ -39,7 +39,9 @@ function compute_Δ_method_2!(g::AbstractGraph)
 		[g.observables.largest_cluster_size[i+1] - g.observables.largest_cluster_size[i]
 		for i in 1:length(g.observables.largest_cluster_size)-1]
 	)
-	g.observables.Δ_method_2 = (t₁-t₀, t₀, t₁) ./ g.n
+	C₀ = g.observables.largest_cluster_size[t₀]
+	C₁ = g.observables.largest_cluster_size[t₁]
+	g.observables.Δ_method_2 = (t₀, t₁, C₀, C₁) ./ g.n
 end
 
 
