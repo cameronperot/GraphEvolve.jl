6208bbb3e34b5796380edd20856fc1d31de5c015
Cameron Perot <cameron.perot@protonmail.ch>
2019-08-08 20:13:53 +0200

    Updated analysis method to address out of bounds error

--- src/analysis_methods.jl
+++ src/analysis_methods.jl
@@ -9,14 +9,21 @@ Extra Information
 * `t₀`: The last step in the evolution process satisfying `g.observables.largest_cluster_size < sqrt(g.n)`
 * `t₁`: The first step in the evolution process satisfying `g.observables.largest_cluster_size > 0.5g.n`
 Returns
-* None, updates `g` in-place
+* `nothing`, updates `g` in-place
 """
 function compute_Δ_method_1!(g::AbstractGraph)
 	t₀ = sum(g.observables.largest_cluster_size .< sqrt(g.n))
 	t₁ = sum(g.observables.largest_cluster_size .<= 0.5g.n) + 1
+
 	C₀ = g.observables.largest_cluster_size[t₀]
-	C₁ = g.observables.largest_cluster_size[t₁]
+	if t₁ > g.t
+		C₁ = NaN
+	else
+		C₁ = g.observables.largest_cluster_size[t₁]
+	end
+
 	g.observables.Δ_method_1 = (t₀, t₁, C₀, C₁) ./ g.n
+	return nothing
 end
 
 
@@ -31,17 +38,20 @@ Extra Information
 * `t₀`: The step in the evolution process where the heterogeneity peaks
 * `t₁`: The step in the evolution process where the size of the largest cluster increases the most
 Returns
-* None, updates `g` in-place
+* `nothing`, updates `g` in-place
 """
 function compute_Δ_method_2!(g::AbstractGraph)
 	t₀ = argmax(g.observables.heterogeneity)
 	t₁ = argmax(
 		[g.observables.largest_cluster_size[i+1] - g.observables.largest_cluster_size[i]
 		for i in 1:length(g.observables.largest_cluster_size)-1]
-	)
+	) + 1
+
 	C₀ = g.observables.largest_cluster_size[t₀]
 	C₁ = g.observables.largest_cluster_size[t₁]
+
 	g.observables.Δ_method_2 = (t₀, t₁, C₀, C₁) ./ g.n
+	return nothing
 end
 
 
@@ -53,7 +63,7 @@ Finalize the observables for the given evolved `g`
 Arguments
 * `g` : An evolved instance of type AbstractGraph
 Returns
-* None, updates `g` in-place
+* `nothing`, updates `g` in-place
 """
 function finalize_observables!(g::AbstractGraph)
 	compute_Δ_method_1!(g)
